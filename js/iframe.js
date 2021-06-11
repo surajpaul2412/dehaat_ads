4bd2e0bb191a4b1d2c55003ebd9fce86d4fff7ee
surajpaul2412 <48125960+surajpaul2412@users.noreply.github.com>
2021-06-11 12:10:03 +0530

    Add files via upload

--- js/iframe.js
+++ js/iframe.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {  
 	$('#iframe-btn').on('shown.bs.modal', function (e) {
 	$('#iframe-btn2').on('shown.bs.modal', function (e) {
+	$('#lead-form').on('shown.bs.modal', function (e) {
 });
