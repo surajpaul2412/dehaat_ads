97b25c3002af1fd35ace5d33978fd37605d40d39
surajpaul <suraj.paul.69@gmail.com>
2021-06-18 03:01:55 +0530

    18june

--- js/iframe.js
+++ js/iframe.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {  
 	$('#iframe-btn').on('shown.bs.modal', function (e) {
 	$('#iframe-btn2').on('shown.bs.modal', function (e) {
-	// $('#iframe-btn3').on('shown.bs.modal', function (e) {
+	$('#iframe-btn3').on('shown.bs.modal', function (e) {
+	$('#iframe-btn4').on('shown.bs.modal', function (e) {
 	$('#lead-form').on('shown.bs.modal', function (e) {
 });
